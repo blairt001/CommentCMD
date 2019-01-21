79fa3400c37de9e9c125cfc61ebc429e90eba307
Eric <eric_maina@hotmail.com>
2019-01-21 13:41:35 +0300

    [starts #163357875] basic structure of the login file

---
+++ login.py
@@ -0,0 +1,17 @@
+
+
+user = input ("Enter your name: ")
+print('Welcome to this CLI ' + user)
+
+print("Please login")
+username = input ("Enter your username: ")
+
+if username == admin:
+
+if username == moderator:
+
+else:
+    
+
+ 
+
