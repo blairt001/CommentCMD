345b2857dfdbfe94c15114ba1ea1d37b275c053f
timothy <timlubanga@gmail.com>
2019-01-21 16:11:15 +0300

    user can create comment

--- user.py
+++ user.py
@@ -1,21 +1,37 @@
 class Comments():
 	def __init__(self,title,content):
 		self.title=title
-		self.content=title
-class User(Comments):
-
-	def __init__(self,title, content, username, password, role="normal"):
-		Comments.__init__(self,title,content)
-		self.username=username
-		self.password=password
-		self.role=role
+		self.content=content
 		self.comments=[]
-
-	def __repr__(self):
-		print("{}".format(self.comments[0]))
 
 	def create_own_comment(self,title):
 		comment=input("please enter comment:")
 		self.comments.append(Comments(title,comment))
+		print("successfully created")
+		print(len(self.comments))
+
+	def edit_own_comment(self):
+		x=input("please enter the comment title:")
+		for key,item in self.comments.items():
+			pass
+class User():
+
+	def __init__(self, username, password, role="normal"):
+		self.username=username
+		self.password=password
+		self.role=role
+		
+
+	def create_user(self):
+		p=input("please enter your name:")
+		w=input("please enter username:")
+		x=input("please enter password:")
+		p=User(w,x)
+		print("user created successfully")
 
 	
+x=User("timo","lubs")
+y=Comments("blog","politics")
+x.create_user()
+y.create_own_comment("title")
+
