a1fa239d5dffb442cb099f50b4ec10fef4fa869c
dennisdnyce <jumaspay@gmail.com>
2019-01-21 15:25:54 +0300

    [start #163358310] Moderator functions

---
+++ home.py
@@ -0,0 +1,56 @@
+import time
+
+
+users = {
+    "moderator": {
+        "password": "jumaspass",
+        "username": "juma"
+    }
+}
+
+
+def validate(form):
+    if len(form) > 0:
+        return False
+    return True
+
+
+def loginauth(username, password):
+    if username in users:
+        if password == users[username]["password"]:
+            print("Login successful")
+            return True
+    return False
+
+
+def login():
+    while True:
+        username = input("Username: ")
+        if not len(username) > 0:
+            print("Username can't be blank")
+        else:
+            break
+    while True:
+        password = input("Password: ")
+        if not len(password) > 0:
+            print("Password can't be blank")
+        else:
+            break
+
+    if loginauth(username, password):
+        return session(username)
+    else:
+        print("Invalid username or password")
+
+
+
+print("Welcome to the system. Please register or login.")
+print("Options: register | login | exit")
+while True:
+    option = input("> ")
+    if option == "login":
+        login()
+    elif option == "exit":
+        break
+    else:
+        print(option + " is not an option")
